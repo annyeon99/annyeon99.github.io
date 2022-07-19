219441ca65e0a5a1c7283f8980f43afcb57cc7b0
Nurul <109496859+annyeon99@users.noreply.github.com>
2022-07-19 16:17:41 +0700

    Add files via upload

---
+++ wisata_bandung.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pertamaku/main_screen.dart';
+import 'package:flutter_pertamaku/model/tourism_place.dart';
+import 'package:flutter_pertamaku/wisata.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wisata Bandung'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return TourismPlaceList();
+          } else if (constraints.maxWidth <= 1200) {
+            return TourismPlaceGrid(gridCount: 4);
+          } else {
+            return TourismPlaceGrid(gridCount: 6);
+          }
+        },
+      ),
+    );
+  }
+}
